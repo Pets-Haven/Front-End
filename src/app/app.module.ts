db7b62d4ef832eb7eae6b9bde0c569d2a9f22f3d
Abdelwahab Mohamed <abdelwahab.mohammed98@gmail.com>
2024-03-28 14:39:13 +0200

    navbar added and files structure set

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
